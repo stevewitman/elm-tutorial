0dc9412f790812f894b761372c688fab6e8e1caa
Steve Witman <stevewitman@gmail.com>
2016-11-21 22:11:59 -0700

    Write functions and type signatures

--- Main.elm
+++ Main.elm
@@ -2,5 +2,16 @@ module Main exposing (..)
 
 import Html exposing (text)
 
+politely : String -> String
+politely phrase =
+  "Excuse me, " ++ phrase
+
+ask : String -> String -> String
+ask thing place =
+  "is there a " ++ thing ++ " in the " ++ place ++ "?"
+
+askPolitelyAboutFish : String -> String
+askPolitelyAboutFish = politely << (ask "fish")
+
 main =
-  text "Hello world"
+  text <| askPolitelyAboutFish "hat"
